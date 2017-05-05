90de3dab655f6c273ba19a906fc27b0430a4ef6a
RM&RISzwj <305108340@qq.com>
2017-05-06 07:54:32 +0800

    commit

---
+++ linux/使用/was缓存.sh
@@ -0,0 +1,3 @@
+find /opt/IBM/WebSphere/AppServer/profiles -type d -path *server-ica* -name jsp
+
+rm -rf /opt/IBM/WebSphere/AppServer/profiles/Custom01/temp/v6demoNode01/server-ica/ica/ica.war/jsp
---
+++ 数据结构变更/ica/新ica/ICA_工业营销_存销比配货箱万支单位切换_张文杰_20170417/ICA_工业营销_存销比配货箱万支单位切换_张文杰_20170417_data.sql
@@ -0,0 +1,8 @@
+--修改ICOM_ID为项目实际工业公司ID
+INSERT INTO ICA_ICOM_PARMS VALUES (
+	${ICOM_ID},
+	'DIST_UM',
+	'W',
+	'配货相关功能默认单位',
+	'20170413',
+	'X：箱，W：万支');
